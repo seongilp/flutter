b329ae9d4bc66f4dfc1bd50f7ec62d756749dc17
Seongil Park <zihado@gmail.com>
2022-03-03 21:15:09 +0900

    hello

---
+++ MiCard/micard/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(backgroundColor: Colors.red, body: Container()),
+    );
+  }
+}
